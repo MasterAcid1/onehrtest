e2211b4f15081d6d8ca5bd119f0a7b8d2743fe48
MasterAcid1 <83093559+MasterAcid1@users.noreply.github.com>
2021-04-27 09:56:52 -0400

    au.js

--- au.js
+++ au.js
@@ -155,7 +155,7 @@ class Client {
 
     loadGUI() {
         $('.agario-promo-container').replaceWith(`
-        <input onchange="localStorage.setItem('botNick', this.value);" id="botNick" maxlength="15" class="form-control" placeholder="Bot Name" value="LOL"></input>
+        <input onchange="localStorage.setItem('botNick', this.value);" id="botNick" maxlength="15" class="form-control" placeholder="Bot Name" value="KrunkerDarezYT"></input>
         <input onchange="localStorage.setItem('botAmount', this.value);" id="BotAmount" maxlength="3" class="form-control" placeholder="Bot Amount" value="150"></input>
 	<button id='button' class='btn btn-danger'>Cannot exceed 200 bots, maximum of 200 bots will spawn even if you put higher. Feel free to PURCHASE a higher plan in GODBots</button>
         <button id='button' class='btn btn-danger'>bots will spawn even if you put higher. Feel free to PURCHASE a higher plan in GODBots</button>
